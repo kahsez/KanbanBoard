7e6cbef2545f798e5bc65ca85536d8ed7e3ec336
kahsez <kahsez@users.noreply.github.com>
2025-05-26 15:37:01 +0200

    build: tests project, install FluentAssertions, install NSubstitute

---
+++ tests/Backend.Tests/KanbanBoard.Backend.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace KanbanBoard.Backend.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
