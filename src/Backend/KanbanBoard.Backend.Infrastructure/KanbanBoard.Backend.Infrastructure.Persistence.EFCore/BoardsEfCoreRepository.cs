b89d4cdc196e43b48ba07a5cf65d47cb110642e0
kahsez <kahsez@users.noreply.github.com>
2025-06-11 02:40:58 +0200

    feat: EFCore repository empty implementation

--- src/Backend/KanbanBoard.Backend.Infrastructure/KanbanBoard.Backend.Infrastructure.Persistence.EFCore/BoardsEfCoreRepository.cs
+++ src/Backend/KanbanBoard.Backend.Infrastructure/KanbanBoard.Backend.Infrastructure.Persistence.EFCore/BoardsEfCoreRepository.cs
@@ -1,6 +1,32 @@
-﻿namespace KanbanBoard.Backend.Infrastructure.Persistence.EFCore;
+﻿using KanbanBoard.Backend.Application.DrivenPorts;
+using KanbanBoard.Backend.Domain;
 
-public class BoardsEfCoreRepository
+namespace KanbanBoard.Backend.Infrastructure.Persistence.EFCore;
+
+public class BoardsEfCoreRepository : IBoardsRepository
 {
-    
+    public Task<Board> Create(Board board)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Board>> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Board?> GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Board> Update(Board board)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<int> Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
 }
